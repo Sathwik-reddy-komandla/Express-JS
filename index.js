7bf38a045a556aa755c7696a8659a2a0aa4ea3fd
sathwik reddy komandla <sathwikreddykomandla@gmail.com>
2023-09-17 11:13:54 +0530

    understanding middlewares

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 import express from 'express';
+// import products from './products.js'
+import userCredentials from './middlewares/logs.js';
+
+
 
 const app=express()
+app.use(userCredentials)
 
+//Query String
+// app.get('/product',(req,res)=>{
+//     const {category,id}=req.query
+//     res.send(`Response ok ${category} and ${id}`)
+// })
 
-//Quey String
+app.get('/',(req,res)=>{
+    res.send("you are auhthenticated")
+})
 
+//sending Json
+// app.get('/products',(req,res)=>{
+//     res.json(products)
+// })
 
 app.listen(3000,()=>{
     console.log("Server Up ")
